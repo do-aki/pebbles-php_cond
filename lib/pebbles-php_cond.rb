41d21c8709deefe744a7462b3c3bf123c3f590ac
do-aki <do.hiroaki@gmail.com>
2012-12-08 01:45:05 +0900

    not use == operation

--- lib/pebbles-php_cond.rb
+++ lib/pebbles-php_cond.rb
@@ -3,32 +3,33 @@ require 'pebbles-php_cond/version'
 module Pebbles
   module PhpCond
     def self.bool(value)
-      return false if value.nil? || value == false || value == 0 || value == 0.0
-      return value != '' && value != '0' if value.is_a?(String)
-      return value.count != 0 if value.respond_to?(:count)
-      return value.to_s.size != 0
+      return false if value.nil? || value.equal?(false) || value.eql?(0) || value.eql?(0.0)
+      return !(value.empty? || value === '0') if value.is_a?(String)
+      return !value.count.eql?(0) if value.respond_to?(:count)
+      return !value.to_s.size.eql?(0)
     end
 
     def self.equal?(rhs, lhs)
-      return true if rhs == lhs
+      return true if rhs.equal?(lhs)
 
-      return bool(lhs) if rhs == true
-      return bool(rhs) if lhs == true
+      return bool(lhs) if rhs.equal?(true)
+      return bool(rhs) if lhs.equal?(true)
 
-      return !bool(lhs) if rhs == false
-      return !bool(rhs) if lhs == false
+      return !bool(lhs) if rhs.equal?(false)
+      return !bool(rhs) if lhs.equal?(false)
 
       return equal_as_numeric(rhs, lhs) if rhs.is_a?(Numeric)
       return equal_as_numeric(lhs, rhs) if lhs.is_a?(Numeric)
 
-      return !(lhs == '0' || bool(lhs)) if (rhs.nil?)
-      return !(rhs == '0' || bool(rhs)) if (lhs.nil?)
+      return !(lhs === '0' || bool(lhs)) if rhs.nil?
+      return !(rhs === '0' || bool(rhs)) if lhs.nil?
 
+      return rhs.eql?(lhs) if rhs.class == lhs.class
       return false if rhs.is_a?(String) || lhs.is_a?(String)
 
-      return true if rhs.is_a?(Enumerable) && lhs.is_a?(Enumerable) && rhs.count == 0 && lhs.count == 0
+      return true if rhs.is_a?(Enumerable) && lhs.is_a?(Enumerable) && rhs.count.eql?(0) && lhs.count.eql?(0)
 
-      return false;
+      return false
     end
 
     def self.identical?(rhs, lhs)
@@ -37,7 +38,7 @@ module Pebbles
 
     private
     def self.equal_as_numeric(own, other)
-        return other.respond_to?(:to_f) && other.to_f == own || other.respond_to?(:to_i) && other.to_i == own
+        return other.respond_to?(:to_f) && other.to_f.<=>(own).equal?(0) || other.respond_to?(:to_i) && other.to_i.equal?(own)
     end
   end
 end
--- lib/pebbles-php_cond/version.rb
+++ lib/pebbles-php_cond/version.rb
@@ -1,5 +1,5 @@
 module Pebbles
   module PhpCond
-    VERSION = '0.0.1'
+    VERSION = '0.0.2'
   end
 end
